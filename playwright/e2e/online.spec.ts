d0fb189a3f74aa1e40a5e19b203e38f8e42bbf9b
unknown <contatoeduardorezes@gmail.com>
2026-02-23 20:45:13 -0300

    Fix: Update landing page URL in online.spec.ts

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Should check if the landing page is loaded', async ({ page }) => {
-  await page.goto('http://localhost:5173');
+  await page.goto('http://localhost:5173/');
 
   await expect(page).toHaveTitle('Velô by Papito');
 });
