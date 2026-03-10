50f499a2aa40f6ef7f5e28684d323637bc745af5
unknown <contatoeduardorezes@gmail.com>
2026-03-10 19:32:34 -0300

    Feature: Add Helper generateOrderCode function to create unique order codes

---
+++ playwright/suport/helpers.ts
@@ -0,0 +1,9 @@
+export function generateOrderCode(): string {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = 'VLO-';
+    for (let i = 0; i < 6; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  }
+  
